079d4732a21d7c1578370887a95373bec0fc8558
Aaron Herrera <aaron4herrera@gmail.com>
2026-06-27 19:28:07 -0500

    Squashed commit of the following:
    commit 62c57c7ef1ed2f8bdf5b2ddf1d86bb995e6f29ac
    Author: Aaron Herrera <aaron4herrera@gmail.com>
    Date:   Sat Jun 27 19:27:47 2026 -0500
    
        Updated README
    
    commit 476573d995641a2064c41ef7cce5d152ff89704e
    Author: Aaron Herrera <aaron4herrera@gmail.com>
    Date:   Sat Jun 27 19:07:39 2026 -0500
    
        Added indexing benchmark for performance tuning
    
    commit 188c3345b534cd124809c178c5bcd6ec879e834e
    Author: Aaron Herrera <aaron4herrera@gmail.com>
    Date:   Wed Jun 24 12:45:07 2026 -0500
    
        Cleaned data, insert/load statements, log table for recording stats/duration
    
    commit a27e3cd67605e5b362246c2ba416af2a61c6df5d
    Author: Aaron Herrera <aaron4herrera@gmail.com>
    Date:   Mon Jun 22 19:53:25 2026 -0500
    
        Installed Python libraries

--- init/01_schema.sql
+++ init/01_schema.sql
@@ -5,29 +5,44 @@ CREATE TABLE vendors (
 );
 
 CREATE TABLE payment_types (
-    payment_type_id   SMALLINT PRIMARY KEY,
-    payment_description VARCHAR(50) NOT NULL
+    payment_type_id      SMALLINT PRIMARY KEY,
+    payment_description   VARCHAR(50) NOT NULL
+);
+
+-- TLC taxi zones (pickup/dropoff location reference). Seeded from
+-- taxi_zone_lookup.csv in 02_seed_zones.sql.
+CREATE TABLE zones (
+    location_id  INT PRIMARY KEY,
+    borough      VARCHAR(30) NOT NULL,
+    zone         VARCHAR(100),
+    service_zone VARCHAR(30)
 );
 
 CREATE TABLE trips (
-    trip_id          SERIAL PRIMARY KEY,
-    vendor_id        INT REFERENCES vendors(vendor_id),
-    pickup_datetime  TIMESTAMP NOT NULL,
-    dropoff_datetime TIMESTAMP,
-    passenger_count  SMALLINT,
-    trip_distance    NUMERIC(8, 2),
-    fare_amount      NUMERIC(10, 2),
-    tip_amount       NUMERIC(10, 2),
-    total_amount     NUMERIC(10, 2),
-    payment_type     SMALLINT REFERENCES payment_types(payment_type_id)
+    trip_id              SERIAL PRIMARY KEY,
+    vendor_id             INT REFERENCES vendors(vendor_id),
+    pickup_datetime       TIMESTAMP NOT NULL,
+    dropoff_datetime      TIMESTAMP,
+    passenger_count       SMALLINT,
+    trip_distance         NUMERIC(8, 2),
+    pickup_location_id    INT NOT NULL REFERENCES zones(location_id),
+    dropoff_location_id   INT NOT NULL REFERENCES zones(location_id),
+    fare_amount           NUMERIC(10, 2),
+    tip_amount            NUMERIC(10, 2),
+    total_amount          NUMERIC(10, 2),
+    payment_type          SMALLINT REFERENCES payment_types(payment_type_id)
 );
 
--- Seed lookup data based on the NYC TLC data dictionary.
+-- Seed lookup data based on the NYC TLC yellow taxi data dictionary
+-- (https://www.nyc.gov/assets/tlc/downloads/pdf/data_dictionary_trip_records_yellow.pdf).
 INSERT INTO vendors (vendor_id, vendor_name) VALUES
-    (1, 'Creative Mobile Technologies'),
-    (2, 'VeriFone Inc.');
+    (1, 'Creative Mobile Technologies, LLC'),
+    (2, 'Curb Mobility, LLC'),
+    (6, 'Myle Technologies Inc'),
+    (7, 'Helix');
 
 INSERT INTO payment_types (payment_type_id, payment_description) VALUES
+    (0, 'Flex Fare trip'),
     (1, 'Credit card'),
     (2, 'Cash'),
     (3, 'No charge'),
